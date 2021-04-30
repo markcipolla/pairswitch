c092a3fc205ececa3ae95b08134a962776ec53ca
Mark Cipolla <mark@markcipolla.com>
2021-04-30 23:12:43 +1000

    More refactoring

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ mod structs;
 use structs::{ Contributor, Commit };
 use itertools::Itertools;
 use serde_json::value::Value;
-use fui::fields::Autocomplete;
+use fui::fields::{Autocomplete, Multiselect};
 use fui::form::FormView;
 
 
@@ -31,7 +31,6 @@ use cursive::{
       Theme,
     },
     views::{
-      TextView,
       ResizedView,
       Dialog,
     }
@@ -39,39 +38,52 @@ use cursive::{
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash)]
 enum BasicColumn {
-    ShortSha,
-    Subject,
-    Authors,
+  ShortSha,
+  Subject,
+  Authors,
 }
 
-impl BasicColumn {
-  fn as_str(&self) -> &str {
-    match *self {
-      BasicColumn::ShortSha => "SHA",
-      BasicColumn::Subject => "Subject",
-      BasicColumn::Authors => "Authors",
-    }
+fn contributors(commits: Vec<Commit>) -> Vec<Contributor> {
+  let people: Vec<Contributor> = commits.iter()
+    .map(|commit| {
+      let mut list = [commit.author.clone()].to_vec();
+      if commit.co_authors.len() > 0 {
+        commit.co_authors.iter().map(|c| list.push(c.clone()));
+      }
+      return list;
+    })
+    .flatten()
+    .unique_by(|c| format!("{}-{}", c.name.clone(), c.email.clone()))
+    .collect();
+
+    return people;
+}
+
+fn co_author_names(commit: &Commit) -> Vec<String> {
+  commit.co_authors.iter().map(|co_author| { co_author.name.clone() }).collect()
+}
+
+fn authors_and_contributors(commit: &Commit) -> Vec<Contributor> {
+  let mut list = [commit.author.clone()].to_vec();
+  if commit.co_authors.len() > 0 {
+    commit.co_authors.iter().map(|c| list.push(c.clone()));
   }
+  return list;
 }
 
-fn authors_and_contributors(commit: &Commit) -> String {
-  let co_authors: Vec<String> = commit.co_authors.iter().map(|co_author| { co_author.name.clone() }).collect();
+fn authors_and_contributors_names(commit: &Commit) -> String {
+  let contributors = authors_and_contributors(commit);
 
-  let author_list: String = if co_authors.len() > 0 {
-    format!("{}, {}", commit.author.clone().name, co_authors.join(", "))
-  } else {
-    format!("{}", commit.author.clone().name)
-  };
-
-  return author_list.to_string();
+  return contributors.iter().map(|contributor| contributor.clone().name).join(", ").to_string();
 }
+
 
 impl TableViewItem<BasicColumn> for Commit {
   fn to_column(&self, column: BasicColumn) -> String {
     match column {
       BasicColumn::ShortSha => self.short_sha.to_string(),
-      BasicColumn::Subject => format!("{}", self.subject),
-      BasicColumn::Authors => format!("{}", authors_and_contributors(self)),
+      BasicColumn::Subject => self.subject.to_string(),
+      BasicColumn::Authors => authors_and_contributors_names(self).to_string(),
     }
   }
 
@@ -82,7 +94,7 @@ impl TableViewItem<BasicColumn> for Commit {
       match column {
         BasicColumn::ShortSha => self.short_sha.cmp(&other.short_sha),
         BasicColumn::Subject => self.subject.cmp(&other.subject),
-        BasicColumn::Authors => authors_and_contributors(self).cmp(&authors_and_contributors(other)),
+        BasicColumn::Authors => authors_and_contributors_names(self).cmp(&authors_and_contributors_names(other)),
       }
     }
 }
@@ -90,13 +102,24 @@ impl TableViewItem<BasicColumn> for Commit {
 fn theme(siv: &Cursive) -> Theme {
   // We'll return the current theme with a small modification.
   let mut theme = siv.current_theme().clone();
-  theme.palette[PaletteColor::Background] = Color::TerminalDefault;
-
+  // theme.palette[PaletteColor::Background] = Color::Rgb(44, 62, 8);
+  theme.palette[PaletteColor::View] = Color::Rgb(236, 240, 241);
+  theme.palette[PaletteColor::Primary] = Color::Rgb(44, 62, 8);
+  // theme.palette[PaletteColor::Secondary] = Color::Rgb(52, 152, 219);
+  // theme.palette[PaletteColor::Tertiary] = Color::Rgb(41, 128, 185);
+  // theme.palette[PaletteColor::TitlePrimary] = Color::Rgb(180,180,180);
+  // theme.palette[PaletteColor::TitleSecondary] = Color::Rgb(200,200,200);
+  // theme.palette[PaletteColor::Highlight] = Color::Rgb(231, 76, 60);
+  // theme.palette[PaletteColor::HighlightInactive] = Color::Rgb(255,255,255);
+  // theme.shadow = false;
   theme
 }
 
+fn cancel_form(s: &mut Cursive) {
+  s.pop_layer();
+}
 
-fn show_data(c: &mut Cursive, data: Value) {
+fn submit_form(c: &mut Cursive, data: Value) {
   c.pop_layer();
   let text = format!("Got data: {:?}", data);
   c.add_layer(Dialog::info(text));
@@ -119,53 +142,32 @@ fn main() {
     });
 
   let commits: Vec<Commit> = interrogate_git_repository();
-  let collaborators: Vec<Contributor> = commits.clone()
-    .iter()
-    .map(|commit| {
-      commit.author.clone()
-    })
-    .unique_by(|c| format!("{}-{}", c.name, c.email))
-    .collect();
 
-
-  let collaborator_names: Vec<String> = collaborators.iter()
-
+  let collaborator_names: Vec<String> = contributors(commits.clone()).iter()
     .map(|collaborator| {
-      collaborator.name.clone().to_string()
+      collaborator.clone().name.to_string()
     })
     .collect();
-
-  let _names_list: Vec<String> = collaborator_names.iter().cloned().unique().collect_vec();
-
 
   table.set_items(commits.clone());
 
-  // table.set_on_sort(|siv: &mut Cursive, column: BasicColumn, order: Ordering| {
-  //   siv.add_layer(
-  //     Dialog::around(TextView::new(format!("{} / {:?}", column.as_str(), order)))
-  //       .title("Sorted by")
-  //       .button("Close", |s| {
-  //         s.pop_layer();
-  //       }),
-  //   );
-  // });
-
   table.set_on_submit(move |siv: &mut Cursive, _row: usize, index: usize| {
-    let _value = siv.call_on_name("table", move |table: &mut TableView<Commit, BasicColumn>| {
-        format!("{:?}", table.borrow_item(index).unwrap())
-      }).unwrap();
+    let commit: &Commit = &commits.clone()[index];
 
-
-      let author_name = &commits.clone()[index].author.name;
-      let form = FormView::new()
-        .field(
-          Autocomplete::new("Author", collaborator_names.clone())
-            .help("help")
-            .initial(author_name)
-            .validator(Required)
-        )
-        .on_submit(show_data);
-      siv.add_layer(Dialog::around(form).full_screen());
+    let author_name = commit.clone().author.name;
+    let form = FormView::new()
+      .field(
+        Autocomplete::new("Author", collaborator_names.clone())
+          .initial(author_name)
+          .validator(Required)
+      )
+      .field(
+        Multiselect::new("Co-author (s)", collaborator_names.clone())
+        .initial(co_author_names(&commit))
+      )
+      .on_submit(submit_form)
+      .on_cancel(cancel_form);
+    siv.add_layer(Dialog::around(form));//.full_screen());
   });
 
   siv.add_layer(
