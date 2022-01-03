2d0a96a3f9817ce2fbfcf985e5c6d89a818d1212
Mark Cipolla <mark@markcipolla.com>
2022-01-03 21:55:20 +1100

    Start building out pair selection process

--- src/pairing_session_start.rs
+++ src/pairing_session_start.rs
@@ -6,7 +6,14 @@ use cursive::views::{
 pub fn start_pairing_session(siv: &mut Cursive) {
   siv.add_layer(Dialog::text("Start a pairing session?")
     .title("Pairswitch")
-    .button("Yes", |s| s.quit())
+    .button("Yes", select_pair)
     .button("No", |s| s.quit())
   );
+}
+
+fn select_pair(siv: &mut Cursive) {
+  siv.pop_layer();
+  siv.add_layer(Dialog::text("Who are you pairing with?")
+    .title("Pairswitch")
+    .button("Finish", |s| s.quit()));
 }
